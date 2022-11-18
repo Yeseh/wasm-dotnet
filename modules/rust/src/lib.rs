202f625454443eab179318f958e31d1620db507b
Jesse Wellenberg <jwellenberg@xpirit.com>
2022-11-18 16:19:10 +0100

    Add ENV var to rust module

--- modules/rust/src/lib.rs
+++ modules/rust/src/lib.rs
@@ -1,11 +1,14 @@
 #[no_mangle]
 pub extern "C" fn hello() {
     let dotnet_version = std::env::var("DOTNET_VERSION")
-    .unwrap_or(String::from("NOT FOUND"));
-    println!("Heyyy Dotnet, 
+        .unwrap_or(String::from("NOT FOUND"));
 
-I see you are using version {}
-And I just wanted to say hello :)
+    println!("
+        Heyyy Dotnet, 
 
-xoxo Rust", dotnet_version);
+        I see you are using version {}
+        And I just wanted to say hello :)
+
+        xoxo Rust", dotnet_version
+    );
 }
