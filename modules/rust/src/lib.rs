47204d1bd7c2fa97e1daa7af6eabeeeb98b76f7d
Jesse Wellenberg <jwellenberg@xpirit.com>
2022-11-18 15:02:18 +0100

    Dotnet hello world with rust

---
+++ modules/rust/src/lib.rs
@@ -0,0 +1,11 @@
+#[no_mangle]
+pub extern "C" fn hello() {
+    let dotnet_version = std::env::var("DOTNET_VERSION")
+    .unwrap_or(String::from("NOT FOUND"));
+    println!("Heyyy Dotnet, 
+
+I see you are using version {}
+And I just wanted to say hello :)
+
+xoxo Rust", dotnet_version);
+}
